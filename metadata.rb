e31cab31b590b29605641bbd5a8148be195686cf
David Dooley <ddooley@Davids-MacBook-Pro.local>
2018-02-15 16:54:09 -0800

    Updating meta version

--- metadata.rb
+++ metadata.rb
@@ -3,18 +3,18 @@ maintainer 'David Dooley'
 maintainer_email 'david.dooley@contrastsecurity.com'
 license 'MIT'
 description 'Installs the Contrast Security agent'
-long_description 'Installs the Contrast Security agent'
-version '0.1.0'
+long_description 'Installs the Contrast Security agent. Contains a Tomcat recipe that creates a script that sets JAVA_OPTS and wraps around the tomcat startup script'
+version '0.2.0'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/ddooley77/contrast_agent/issues'
+issues_url 'https://github.com/ddooley77/contrast_agent/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/ddooley77/contrast_agent'
+source_url 'https://github.com/ddooley77/contrast_agent'
